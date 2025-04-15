118bdafefe7680a0a9f188509856698081b902bd
Filipe Góis <fythoy@gmail.com>
2025-04-15 17:12:08 -0300

    .

--- 24.py
+++ 24.py
@@ -1,1 +1,13 @@
-asdf
+salarioAtual = float(input("Digite o salário atual: "))
+
+if salarioAtual < 500:
+    salarioAtual *= 1.15
+    print(f"O novo salário é de {salarioAtual}")
+
+elif salarioAtual >= 500 and salarioAtual <= 1000:
+    salarioAtual *= 1.10
+    print(f"O novo salário é de {salarioAtual}")
+
+else:
+    salarioAtual *= 1.05
+    print(f"O novo salário é de {salarioAtual}")
